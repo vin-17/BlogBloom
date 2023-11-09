f27434d91af77efae728cc5c698e9825e6bf0c78
Vineet Singh <vineet.yo3@gmail.com>
2023-11-09 14:42:25 +0530

    server side exception handled

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -16,7 +16,7 @@ import Image from "next/image";
 // };
 const getData = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/categories", {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/categories`, {
       cache: "no-store",
     });
 
