75cd0064d0aa89e7bff37d3b9483e867b5681a38
Vineet Singh <vineet.yo3@gmail.com>
2023-11-09 15:09:05 +0530

    serverside error -- providers

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import GithubProvider from "next-auth/providers/github";
+// import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "./connect";
 import { getServerSession } from "next-auth";
@@ -11,10 +11,10 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
   ],
 };
 
